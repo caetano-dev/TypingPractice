d819ea5edae806d6fa02cc179b4e60fdf777cabc
Pedro Pires <pcaetanop@gmail.com>
2020-12-16 23:07:48 -0300

    Fixing path

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("static").then((cache) => {
-      return cache.addAll(["./", "./styles.css", "./logo.png"]);
+      return cache.addAll(["./", "./src/styles.css", "./logo.png"]);
     })
   );
 });
